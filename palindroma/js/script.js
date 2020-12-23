e4f2b85823eaa1065498d521da6370c93da305c3
Francesco <francescofogliajj@gmail.com>
2020-12-23 17:32:20 +0100

    Commit #2

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -16,3 +16,24 @@ if (parolaUtente == palindroma) {
 } else {
   alert("La parola non è palindroma");
 }
+
+
+// var parolaUtente = prompt("Inserisci una parola").toLowerCase();
+//
+// function roveciaParola(parola) {
+//   var parolaRovesciata = "";
+//
+//   for (var i = parola.length -1; i >= 0; i--) {
+//     parolaRovesciata += parola[i]
+//   }
+//
+//   return parolaRovesciata;
+// }
+//
+// var parolaGirata = roveciaParola(parolaUtente);
+//
+// if (parolaUtente == parolaGirata) {
+//   alert("La parola è palindroma");
+// } else {
+//   alert("La parola non è palindroma");
+// }
